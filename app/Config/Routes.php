909a2fff2de2d70b2368f00e02ddbe623ab5638c
izfaha <rasberry006@gmail.com>
2026-04-04 21:32:22 +0700

    belajar-ci

---
+++ app/Config/Routes.php
@@ -0,0 +1,18 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+use App\Controllers\ProdukController;
+use App\Controllers\TransaksiController;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+// using array callable syntax
+
+// produk route 
+$routes->get('/produk', [ProdukController::class, 'index']);
+
+// keranjang route
+$routes->get('/keranjang', [TransaksiController::class, 'index']);
---
+++ app/Views/v_produk.php
@@ -0,0 +1,1 @@
+<p>Ini Produk page</p>
